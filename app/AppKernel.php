f75557a4c50ed855ec095c6927747eee76e0413d
Miro Cillik <miro@keboola.com>
2013-04-17 15:08:48 +0200

    some deploy shit is going on

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-
 			new Syrup\CoreBundle\SyrupCoreBundle(),
             new Syrup\ComponentBundle\SyrupComponentBundle(),
 			new Syrup\SlevomatBundle\SyrupSlevomatBundle(),
