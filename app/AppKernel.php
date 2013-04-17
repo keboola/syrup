1f4be1e01bfea13d55be84c9f933841fbd9dfceb
Miro Cillik <miro@keboola.com>
2013-04-17 15:18:59 +0200

    temporarily removed CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new Syrup\CoreBundle\SyrupCoreBundle(),
+//			new Syrup\CoreBundle\SyrupCoreBundle(),
             new Syrup\ComponentBundle\SyrupComponentBundle(),
 			new Syrup\SlevomatBundle\SyrupSlevomatBundle(),
 	    	new Syrup\DbBundle\SyrupDbBundle(),
