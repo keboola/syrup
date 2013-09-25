14c3c6f1d7a845c8c06453abe076f1b4c6e38a7d
Miro Cillik <miro@keboola.com>
2013-09-25 10:54:19 +0200

    add deploy user

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,6 +3,7 @@ set :domain,            "ch-data.keboola.com"
 set :deploy_to,         "/www/syrup"
 set :app_path,          "app"
 set :ssh_options,       {:forward_agent => true}
+# set :user,              "deploy"
 
 set :shared_files,      ["app/config/parameters.yml","composer.json"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads"]
@@ -17,7 +18,7 @@ role :app,              domain                         # This may be the same as
 role :db,               domain, :primary => true       # This is where Symfony2 migrations will run
 
 set  :use_sudo,         false
-set  :keep_releases,    3
+set  :keep_releases,    5
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
