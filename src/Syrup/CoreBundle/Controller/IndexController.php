b3f0d19d1b083b8a7f7e14aa07f88d84568cb2c7
Miro Cillik <miro@keboola.com>
2014-03-06 12:27:32 +0000

    fix: Index action

--- src/Syrup/CoreBundle/Controller/IndexController.php
+++ src/Syrup/CoreBundle/Controller/IndexController.php
@@ -8,18 +8,10 @@
 
 namespace Syrup\CoreBundle\Controller;
 
-use Composer\Config;
-use Composer\Factory;
-use Composer\IO\NullIO;
-use Composer\Json\JsonFile;
-use Composer\Package\Locker;
-use Composer\Package\Package;
-use Composer\Repository\ComposerRepository;
-use Composer\Repository\FilesystemRepository;
-use Composer\Repository\InstalledFilesystemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class IndexController extends Controller
 {
@@ -32,18 +24,23 @@ class IndexController extends Controller
 	{
 		$rootPath = str_replace('web/../', '', ROOT_PATH);
 
-		$installedJson = new JsonFile($rootPath . '/vendor/composer/installed.json');
-		$repo = new FilesystemRepository($installedJson);
+		$filepath = $rootPath . '/../../composer/installed.json';
+
+		if (!file_exists($filepath)) {
+			$filepath = $rootPath . '/vendor/composer/installed.json';
+		}
+
+		$installedJson = file_get_contents($filepath);
+
+		$jsonDecoder = new JsonDecode(true);
+		$installedArr = $jsonDecoder->decode($installedJson, JsonEncoder::FORMAT);
 
 		$syrupComponents = array();
-
-		/** @var Package $package */
-		foreach ($repo->getPackages() as $package) {
-
-			$nameArr = explode("/", $package->getPrettyName());
+		foreach ($installedArr as $package) {
+			$nameArr = explode("/", $package['name']);
 
 			if ($nameArr[0] == 'syrup' || $nameArr[0] == 'keboola') {
-				$syrupComponents[$package->getPrettyName()] = $package->getPrettyVersion();
+				$syrupComponents[$package['name']] = $package['version'];
 			}
 		}
 
