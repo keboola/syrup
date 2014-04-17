8c541e260f7dd087ed9932589fd32cde9bf2fc36
Miro Cillik <miro@keboola.com>
2014-04-17 13:06:24 +0000

    fix: autoload.php and composer.json

--- app/autoload.php
+++ app/autoload.php
@@ -10,10 +10,10 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 //@todo somehow detect whether this bundle is in vendor or not
 
 // is in vendor
-//$loader = require __DIR__.'/../../../autoload.php';
+$loader = require __DIR__.'/../../../autoload.php';
 
 // is not
-$loader = require __DIR__.'/../vendor/autoload.php';
+//$loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
