ea3ad24b1c0b787f4f8436d10d5853c287fc855f
Ondrej Vana <ondrej.vana@keboola.com>
2014-10-22 06:27:11 -0700

    Rewrite Syrup\CoreBundle\DeploymentHandler\ScriptHandler  - If in PROD mode, always download from S3 'syrup-configs'  - If in DEV mode AND Interactive, ask whether to download from DEV S3 or ask for a path  - If in DEV mode and not Interactive OR Interactive and asked to download from S3, download it from 'syrup-configs-testing'

--- src/Syrup/CoreBundle/DeploymentHandler/ScriptHandler.php
+++ src/Syrup/CoreBundle/DeploymentHandler/ScriptHandler.php
@@ -8,34 +8,93 @@ use Aws\S3\S3Client;
 
 class ScriptHandler
 {
-	const SHARED_PARAMETERS_PATH = "vendor/keboola/syrup/app/config/parameters_shared.yml";
+	const PARAMETERS_DIR = "vendor/keboola/syrup/app/config/";
 
-    public static function getSharedParameters(Event $event)
-    {
-		if ($event->isDevMode()) {
-			// Ask for the file & copy
-			/** @var IOInterface $io */
-			$io = $event->getIO();
+	public static function getSharedParameters(Event $event)
+	{
+		self::getFile($event, "parameters_shared.yml", "syrup/parameters_shared.yml");
+	}
 
-			$paramsFile = './parameters_shared.yml';
-			if (!file_exists($paramsFile)) {
-				if (file_exists('../../shared/parameters_shared.yml')) {
-					$paramsFile = '../../shared/parameters_shared.yml';
-				} else {
-					$io->write("<error>File <options=bold;fg=yellow>{$paramsFile}</options=bold;fg=yellow> not found!</error>");
-
-					$paramsFile = $io->ask('<comment>Path to "parameters_shared.yml" to use for the development env:</comment> ', $paramsFile);
-				}
-			}
-
-			copy($paramsFile, self::SHARED_PARAMETERS_PATH);
+	public static function getParameters(Event $event)
+	{
+		$extra = $event->getComposer()->getPackage()->getExtra();
+		if (empty($extra['syrup-app-name'])) {
+			// interactive if $event->getIO()->isInteractive() ?
+			throw new \Exception('The app name (ie "ex-dummy") has to be set in composer.json "extra": {"syrup-app-name": "ex-dummy"}');
 		} else {
-			$client = S3Client::factory();
-			$client->getObject(array(
-				'Bucket' => 'keboola-configs',
-				'Key'    => 'syrup/parameters_shared.yml',
-				'SaveAs' => self::SHARED_PARAMETERS_PATH
-			));
+			$appName = $extra['syrup-app-name'];
 		}
-    }
+
+		$appName = $event->getComposer()->getPackage()->getExtra()['syrup-app-name'];
+
+		self::getFile($event, "parameters.yml", "syrup/{$appName}/parameters.yml");
+	}
+
+	protected static function getFile(Event $event, $filename, $s3key)
+	{
+		if ($event->isDevMode()) {
+			if ($event->getIO()->isInteractive()
+				&& !$event->getIO()->askConfirmation("<comment>Get <question>{$filename}</question> from development S3 bucket? [<options=bold>y</options=bold>/n]: </comment>", true)
+			) {
+				self::getFromIO($event, $filename);
+			} else {
+				self::getFromS3($s3key, self::PARAMETERS_DIR . $filename, true);
+			}
+		} else {
+			self::getFromS3($s3key, self::PARAMETERS_DIR . $filename);
+		}
+	}
+
+	/**
+	 * @brief Ask for a path to a file
+	 *
+	 * @param Event $event
+	 * @param string $filename parameters.yml or parameters_shared.yml
+	 * @param string $pathname file to look for before asking in IO
+	 * @return void
+	 */
+	protected static function getFromIO(Event $event, $filename, $pathname = "")
+	{
+		if (!file_exists($pathname)) {
+			$io = $event->getIO();
+			if ($io->isInteractive()) {
+				$try = 0;
+				while (!file_exists($pathname)) {
+					if ($try >= 3) {
+						throw new \InvalidArgumentException("3 attempts exhausted, {$filename} not found!");
+					}
+					if ($try > 0) {
+						$io->write("<error>File<options=bold;fg=yellow> {$pathname} </options=bold;fg=yellow>does not exist!</error>");
+					}
+
+					$try++;
+					$pathname = $io->ask("<comment>Path to '{$filename}':</comment> ", $pathname);
+				}
+			} else {
+				throw new \Exception("Failed to retrieve {$filename} from IO: Input is not interactive");
+			}
+		}
+
+		$dest = self::PARAMETERS_DIR . $filename;
+		copy($pathname, $dest);
+		$event->getIO()->write("<info>File {$pathname} copied to {$dest}</info>");
+	}
+
+	/**
+	 * @brief Download a file from S3 to $path
+	 *
+	 * @param string $key "path" to a file on S3
+	 * @param string $path Local path to save the file within app
+	 * @param bool $dev Development environment
+	 * @return void
+	 */
+	protected static function getFromS3($key, $path, $dev = false)
+	{
+		$client = S3Client::factory();
+		$client->getObject(array(
+			'Bucket' => (bool) $dev ? 'keboola-configs-testing' : 'keboola-configs',
+			'Key'	=> $key,
+			'SaveAs' => $path
+		));
+	}
 }
