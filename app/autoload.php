cf0ff7837669702ea7e3e0f69fdfe99a3249f339
Jakub Matejka <jakub@keboola.com>
2015-02-11 14:31:29 +0000

    fix: upload too long log message to s3 + add autoload file for symfony bootstrap builder

---
+++ app/autoload.php
@@ -0,0 +1,8 @@
+<?php
+use Composer\Autoload\ClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+/** @var $loader ClassLoader */
+$loader = require __DIR__.'/../../../autoload.php';
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+return $loader;
