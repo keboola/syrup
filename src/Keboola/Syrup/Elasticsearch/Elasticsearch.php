a7ac600ef8b70ce57a3bf86d5fd6427a5ba27287
Jakub Matejka <jakub@keboola.com>
2015-02-24 09:45:04 +0000

    feat: improve elasticsearch mapping templates

--- src/Keboola/Syrup/Elasticsearch/Elasticsearch.php
+++ src/Keboola/Syrup/Elasticsearch/Elasticsearch.php
@@ -12,32 +12,58 @@ use Keboola\Syrup\Exception\ApplicationException;
 class Elasticsearch
 {
 
+    public static function getMainMapping($rootDir)
+    {
+        $mappingFile = realpath($rootDir.'/Resources/Elasticsearch/mapping_template.json');
+        $mappingJson = file_get_contents($mappingFile);
+        $mapping = json_decode($mappingJson, true);
+        if ($mapping === null) {
+            throw new ApplicationException('Error in Syrup template mapping, check '.$mappingFile);
+        }
+        return $mapping;
+    }
+
+    public static function getCustomMapping($mappingFile)
+    {
+        $customMappingJson = file_get_contents($mappingFile);
+        $customMapping = json_decode($customMappingJson, true);
+        if ($customMapping === null) {
+            throw new ApplicationException("Error in component's mapping, check ".realpath($mappingFile));
+        }
+        return $customMapping;
+    }
+
+    /**
+     * @deprecated
+     */
+    public static function getCustomMappingDeprecated($mappingFile)
+    {
+        $customMappingJson = file_get_contents($mappingFile);
+        $startTagPosition = strpos($customMappingJson, '{% block params %}') + 18;
+        $endTagPosition = strpos($customMappingJson, '{% endblock %}');
+        $customMappingJson = substr($customMappingJson, $startTagPosition, $endTagPosition - $startTagPosition); // remove twig tags
+        $customMappingJson = rtrim($customMappingJson, ","); // remove trailing comma
+        $customMappingJson = '{'.$customMappingJson.'}'; // make valid object
+        $customMapping = json_decode($customMappingJson, true);
+        if ($customMapping === null) {
+            throw new ApplicationException("Error in component's mapping, check ".realpath($mappingFile));
+        }
+        return $customMapping;
+    }
+
     public static function getMapping($rootDir)
     {
-        $template = file_get_contents($rootDir.'/Resources/Elasticsearch/mapping_template.json');
+        $mapping = self::getMainMapping($rootDir);
 
         if (file_exists($rootDir.'/../../../../Resources/Elasticsearch/mapping.json')) {
-            $customTemplate = file_get_contents($rootDir . '/../../../../Resources/Elasticsearch/mapping.json');
-        } elseif (file_exists($rootDir.'/../../../../Resources/views/Elasticsearch/mapping.json')) {
-            // deprecated location
-            $customTemplate = file_get_contents($rootDir.'/../../../../Resources/views/Elasticsearch/mapping.json');
+            $customMapping = self::getCustomMapping($rootDir.'/../../../../Resources/Elasticsearch/mapping.json');
+            $mapping['mappings']['jobs']['properties'] = array_merge($mapping['mappings']['jobs']['properties'], $customMapping);
         } elseif (file_exists($rootDir.'/../../../../Resources/views/Elasticsearch/mapping.json.twig')) {
-            // deprecated format
-            $customTemplate = file_get_contents($rootDir . '/../../../../Resources/views/Elasticsearch/mapping.json.twig');
-            $startTagPosition = strpos($customTemplate, '{% block params %}') + 18;
-            $endTagPosition = strpos($customTemplate, '{% endblock %}');
-            $customTemplate = substr($customTemplate, $startTagPosition, $endTagPosition - $startTagPosition);
-        } else {
-            $customTemplate = file_get_contents($rootDir.'/Resources/Elasticsearch/mapping_default.json');
+            // deprecated format, remove soon
+            $customMapping = self::getCustomMappingDeprecated($rootDir.'/../../../../Resources/views/Elasticsearch/mapping.json.twig');
+            $mapping['mappings']['jobs']['properties'] = array_merge($mapping['mappings']['jobs']['properties'], $customMapping);
         }
 
-        $mappingJson = str_replace('"params": {},', $customTemplate, $template);
-
-        $json = json_decode($mappingJson, true);
-        if ($json === null) {
-            throw new ApplicationException("Error in mapping, check your mapping syntax");
-        }
-
-        return $json;
+        return $mapping;
     }
 }
