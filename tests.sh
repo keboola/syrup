2b0801e22f71ddcecc4919edd00d0b1cb9297b21
odin <odinuv@gmail.com>
2020-06-14 16:07:03 +0200

    chore: memory override?

--- tests.sh
+++ tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+/sbin/mkswap /var/swap.1
+/sbin/swapon /var/swap.1
+
 echo "Starting tests" >&1
 php --version \
     && composer --version \
